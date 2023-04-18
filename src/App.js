d32a8e893151b7d4fb449330fc6294764c59a088
vaibhav <vpatil7382@gmail.com>
2023-04-18 19:57:31 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Hero from './componant/Hero/Hero';
+import Programs from './componant/Programs/Programs';
+import Reasons from './componant/Reasons/Reasons';
 
 function App() {
   return (
    <div className='App'>
 <Hero/>
+<Programs/>
+<Reasons/>
    </div>
   );
 }
--- src/componant/Hero/Hero.jsx
+++ src/componant/Hero/Hero.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import Header from "../Header/Header";
 import "./Hero.css";
+import hero_image from "../../assets/hero_image.png"
+import hero_image_back from "../../assets/hero_image_back.png"
+import Heart from "../../assets/heart.png"
+import Calories from "../../assets/calories.png"
 const Hero = () => {
   return (
     <div className="hero">
@@ -42,8 +46,28 @@ const Hero = () => {
   </div>
  
 </div>
+{/* hero button */}
+<div className="hero-buttons">
+<button className="btn">Get Started</button>
+<button className="btn">Learn More</button>
 </div>
-      <div className="right-h">right side</div>
+</div>
+      <div className="right-h">
+
+        <button className="btn">Join Now</button>
+        <div className="heart-rate">
+          <img src={Heart} alt="" />
+          <span>Heart Rate</span>
+          <span>116 bpm</span>
+        </div>
+        <img src={hero_image} alt="" className="hero-image" />
+        <img src={hero_image_back} alt="" className="hero-image-back"/>
+        <div className="calories">
+          <img src={Calories} alt="" />
+          <span>Calories Burned</span>
+          <span>220 kcal</span>
+        </div>
+      </div>
     </div>
   );
 };
