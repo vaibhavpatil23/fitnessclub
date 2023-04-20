ce219b7050dbbe1d6a9e7f38eb54fd6eff591801
vaibhav <vpatil7382@gmail.com>
2023-04-20 18:30:00 +0530

    total responsive webside

--- src/componant/Header/Header.jsx
+++ src/componant/Header/Header.jsx
@@ -1,20 +1,59 @@
-import React from 'react'
-import Logo from '../../assets/Logo.png'
-import './Header.css'
+import React, { useState } from "react";
+import Logo from "../../assets/Logo.png";
+import "./Header.css";
+import Bars from "../../assets/bars.png";
+import { Link } from "react-scroll";
 const Header = () => {
+  const mobile = window.innerWidth <= 768 ? true : false;
+  const [menuOpened, setMenuOpened] = useState(false);
+
   return (
-    <div className='header'>
-<img src={Logo} alt="" className='Logo' />
-
-<ul className='header-menu'>
-    <li>Home</li>
-    <li>Program</li>
-    <li>Why us</li>
-    <li>Plans</li>
-    <li>Testimonial</li>
-</ul>
+    <div className="header">
+      <img src={Logo} alt="" className="Logo" />
+      {menuOpened === false && mobile === true ? (
+        <div
+          style={{
+            backgroundColor: "var(--appColor)",
+            padding: "0.5rem",
+            borderRadius: "5px",
+          }}
+          onClick={() => setMenuOpened(true)}
+        >
+          <img
+            src={Bars}
+            alt=""
+            style={{ width: "1.5rem", height: "1.5rem" }}
+          />
+        </div>
+      ) : (
+        <ul className="header-menu">
+          <li>
+            <Link
+              onClick={() => setMenuOpened(false)}
+              to="home"
+              span={true}
+              smooth={true}
+            >
+              Home
+            </Link>
+          </li>
+          <li onClick={() => setMenuOpened(false)}>Program</li>
+          <li onClick={() => setMenuOpened(false)}>Why us</li>
+          <li onClick={() => setMenuOpened(false)}>Plans</li>
+          <li>
+            <Link
+              onClick={() => setMenuOpened(false)}
+              to="testimonials"
+              span={true}
+              smooth={true}
+            >
+              Testimonial
+            </Link>
+          </li>
+        </ul>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/componant/Hero/Hero.jsx
+++ src/componant/Hero/Hero.jsx
@@ -8,16 +8,17 @@ import Calories from "../../assets/calories.png";
 import { motion } from "framer-motion";
 
 const Hero = () => {
-  const transition = { type: "spring", duration: 1 };
+  const transition = { type: "spring", duration: 3 };
+  const mobile = window.innerWidth<=768 ? true:false
   return (
-    <div className="hero">
+    <div className="hero" id="home">
       <div className="blur hero-blur"></div>
       <div className="left-h">
         <Header />
         {/* the best ad */}
         <div className="the-best-ad">
           <motion.div
-            initial={{ left: "238px" }}
+            initial={{ left: mobile? "165px":"238px" }}
             whileInView={{ left: "8px" }}
             transition={{ ...transition, type: tween }}
           ></motion.div>
